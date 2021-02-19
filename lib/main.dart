936ff8ce8837226ae523acc023a42a82c1435549
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-19 14:18:21 +0800

    添加AppBar圖標按鈕

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,19 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation',
+          onPressed: () => debugPrint('Navigation button is pressed.'),
+        ),
         title: Text('Flutter Demo'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: () => debugPrint('Search button is pressed.'),
+          ),
+        ],
         elevation: 0.0,
       ),
       body: null,
