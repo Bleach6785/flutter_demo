598e45ce06cd6aa9117ff87f48b875b4cd72cf83
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-24 10:38:35 +0800

    RichText:行內多樣式的文字

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 
 class BasicDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: 'Flutter Demo',
+        style: TextStyle(
+          color: Colors.deepPurpleAccent,
+          fontSize: 34.0,
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.w100,
+        ),
+        children: [
+          TextSpan(
+            text: '.net',
+            style: TextStyle(
+              fontSize: 17.0,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TextDemo extends StatelessWidget {
   final TextStyle _textStyle = TextStyle(
     fontSize: 16.0,
   );
-
   final String _author = '李白';
   final String _title = '將進酒';
+
   @override
   Widget build(BuildContext context) {
     return Text(
