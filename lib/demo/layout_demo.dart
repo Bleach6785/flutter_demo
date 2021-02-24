c65022ecae2cbe01a83ee597d577ac0c491c659a
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-24 15:47:10 +0800

    Stack:一摞( ㄌㄨㄛˋ )小部件

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -7,40 +7,100 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(
-            width: 200.0,
-            height: 300.0,
-            child: Container(
-              alignment: Alignment(0.0, -0.8),
-              // alignment: Alignment.topCenter,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+          Stack(
+            alignment: Alignment.topLeft,
+            children: [
+              SizedBox(
+                width: 200.0,
+                height: 300.0,
+                child: Container(
+                  alignment: Alignment(0.0, -0.8),
+                  // alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(3, 54, 255, 1.0),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
               ),
-              child: Icon(
-                Icons.ac_unit,
-                color: Colors.white,
-                size: 32.0,
+              SizedBox(
+                height: 32.0,
               ),
-            ),
-          ),
-          SizedBox(
-            height: 32.0,
-          ),
-          SizedBox(
-            width: 100.0,
-            height: 100.0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+              SizedBox(
+                width: 100.0,
+                height: 100.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(3, 54, 255, 1.0),
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(
+                      colors: [
+                        Color.fromRGBO(7, 102, 255, 1.0),
+                        Color.fromRGBO(3, 54, 255, 1.0),
+                      ],
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.brightness_2,
+                    color: Colors.white,
+                    size: 32.0,
+                  ),
+                ),
               ),
-              child: Icon(
-                Icons.brightness_2,
-                color: Colors.white,
-                size: 32.0,
+              Positioned(
+                right: 20.0,
+                top: 20.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 16.0,
+                ),
               ),
-            ),
+              Positioned(
+                right: 20.0,
+                top: 120.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 16.0,
+                ),
+              ),
+              Positioned(
+                right: 70.0,
+                top: 180.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 16.0,
+                ),
+              ),
+              Positioned(
+                right: 30.0,
+                top: 230.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 16.0,
+                ),
+              ),
+              Positioned(
+                right: 90.0,
+                bottom: 20.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 16.0,
+                ),
+              ),
+              Positioned(
+                right: 4.0,
+                bottom: -4.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 16.0,
+                ),
+              ),
+            ],
           ),
         ],
       ),
