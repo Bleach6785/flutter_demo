0b875247c65407862f2a2bc53ddce87062739d31
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-25 12:10:23 +0800

    GridView.extent

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -3,6 +3,42 @@ import 'package:flutter/material.dart';
 import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridViewExtentDemo();
+  }
+}
+
+class GridViewExtentDemo extends StatelessWidget {
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (int index) {
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text(
+          'Item $index',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.grey,
+          ),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent(
+      maxCrossAxisExtent: 150.0,
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      scrollDirection: Axis.horizontal,
+      children: _buildTiles(100),
+    );
+  }
+}
+
+class GridViewCountDemo extends StatelessWidget {
   List<Widget> _buildTiles(int length) {
     return List.generate(length, (int index) {
       return Container(
