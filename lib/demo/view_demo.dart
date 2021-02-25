576d071e650b225c93d758e05d8c432739d79e24
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-25 11:46:58 +0800

    PageView.builder 按需生成頁面

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,6 +1,57 @@
 import 'package:flutter/material.dart';
 
+import '../model/post.dart';
+
 class ViewDemo extends StatelessWidget {
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      children: [
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                posts[index].name,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                posts[index].bloodType,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _pageItemBuilder,
+    );
+  }
+}
+
+class PageViewDemo extends StatelessWidget {
+  const PageViewDemo({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return PageView(
