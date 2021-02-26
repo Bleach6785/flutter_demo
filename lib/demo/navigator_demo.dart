71b5da0b7ba0b2ebe84cadb98317bc575c6f568f
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-26 11:14:12 +0800

    打開新頁面並返回(push與pop):準備

---
+++ lib/demo/navigator_demo.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class NavigatorDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton(
+              child: Text('Home'),
+              onPressed: null,
+            ),
+            FlatButton(
+              child: Text('About'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page extends StatelessWidget {
+  final String title;
+
+  Page({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
